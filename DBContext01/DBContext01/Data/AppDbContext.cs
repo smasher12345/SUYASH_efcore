e0eeadc3537c69730e4b3cce6c2febf7800484db
sonampedgaonkar <sonampedgaonkar08@gmail.com>
2025-06-09 07:47:37 +0530

    DbContext Creation for Entity framework

---
+++ DBContext01/DBContext01/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace DBContext01.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+       
+    }
+}
